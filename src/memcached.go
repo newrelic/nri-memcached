783a9609a9f5094172bf86ac97c7dcadf153001d
Camden Cheek <ccheek22@gmail.com>
2018-10-26 15:28:37 -0400

    Working without authentication

--- src/memcached.go
+++ src/memcached.go
@@ -1,73 +1,135 @@
 package main
 
 import (
-	sdkArgs "github.com/newrelic/infra-integrations-sdk/args"
-	"github.com/newrelic/infra-integrations-sdk/data/event"
-	"github.com/newrelic/infra-integrations-sdk/data/metric"
-	"github.com/newrelic/infra-integrations-sdk/integration"
+	//sdkArgs "github.com/newrelic/infra-integrations-sdk/args"
+	//"github.com/newrelic/infra-integrations-sdk/data/event"
+	//"github.com/newrelic/infra-integrations-sdk/data/metric"
+	//"github.com/newrelic/infra-integrations-sdk/integration"
+	//"encoding/binary"
+	"bufio"
+	"bytes"
+	"fmt"
+	"github.com/lunixbochs/struc"
+	"net"
 )
 
-type argumentList struct {
-	sdkArgs.DefaultArgumentList
+// More info about the binary protocol here: https://github.com/memcached/memcached/wiki/BinaryProtocolRevamped
+
+// RequestHeader is a marshalling struct for the memcached binary header format
+type RequestHeader struct {
+	Magic           byte
+	Opcode          byte
+	KeyLength       int `struc:"int16"`
+	ExtrasLength    int `struc:"int8"`
+	DataType        byte
+	VBucketID       int `struc:"int16"`
+	TotalBodyLength int `struc:"int32"`
+	Opaque          [4]byte
+	CAS             [8]byte
+}
+
+// ResponseHeader is a marshalling struct for the memcached binary header format
+type ResponseHeader struct {
+	Magic           byte
+	Opcode          byte
+	KeyLength       int `struc:"int16"`
+	ExtrasLength    int `struc:"int8"`
+	DataType        byte
+	Status          int `struc:"int16"`
+	TotalBodyLength int `struc:"int32"`
+	Opaque          [4]byte
+	CAS             [8]byte
 }
 
 const (
-	integrationName    = "com.newrelic.memcached"
-	integrationVersion = "0.1.0"
-)
-
-var (
-	args argumentList
+	headerSize = 24
 )
 
 func main() {
-	// Create Integration
-	i, err := integration.New(integrationName, integrationVersion, integration.Args(&args))
-	panicOnErr(err)
-
-	// Create Entity, entities name must be unique
-	e1, err := i.Entity("instance-1", "custom")
-	panicOnErr(err)
-
-	// Add Event
-	if args.All() || args.Events {
-		err = e1.AddEvent(event.New("restart", "status"))
-		panicOnErr(err)
-	}
-
-	// Add Inventory item
-	if args.All() || args.Inventory {
-		err = e1.SetInventoryItem("instance", "version", "3.0.1")
-		panicOnErr(err)
-	}
-
-	// Add Metric
-	if args.All() || args.Metrics {
-		m1 := e1.NewMetricSet("CustomSample")
-		err = m1.SetMetric("some-data", 1000, metric.GAUGE)
-		panicOnErr(err)
-	}
-
-	// Create another Entity
-	e2, err := i.Entity("instance-2", "custom")
-	panicOnErr(err)
-
-	if args.All() || args.Inventory {
-		err = e2.SetInventoryItem("instance", "version", "3.0.4")
-		panicOnErr(err)
-	}
-
-	if args.All() || args.Metrics {
-		m2 := e2.NewMetricSet("CustomSample")
-		err = m2.SetMetric("some-data", 2000, metric.GAUGE)
-		panicOnErr(err)
-	}
-
-	panicOnErr(i.Publish())
+	items := "items"
+	slabs := "slabs"
+	getStats(nil)
+	getStats(&items)
+	getStats(&slabs)
 }
 
-func panicOnErr(err error) {
+// TODO authentication
+func getStats(key *string) {
+
+	var keyBytes []byte
+	var keyLen int
+	if key != nil {
+		keyBytes = []byte(*key)
+		keyLen = len(keyBytes)
+	} else {
+		keyBytes = []byte{}
+		keyLen = 0
+	}
+
+	sendHeader := &RequestHeader{
+		Magic:           0x80,
+		Opcode:          0x10,
+		KeyLength:       keyLen,
+		ExtrasLength:    0x00,
+		DataType:        0x00,
+		VBucketID:       0x0000,
+		TotalBodyLength: keyLen,
+		Opaque:          [4]byte{0x01, 0x01, 0x01, 0x01},
+		CAS:             [8]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+	}
+
+	var sendBuf bytes.Buffer
+
+	err := struc.Pack(&sendBuf, sendHeader)
 	if err != nil {
-		panic(err)
+		println(err.Error())
+	}
+	_, err = sendBuf.Write(keyBytes)
+	if err != nil {
+		println(err.Error())
+	}
+
+	conn, err := net.Dial("tcp", "mc-14-1:11211")
+	if err != nil {
+		println(err.Error())
+	}
+
+	_, err = conn.Write(sendBuf.Bytes())
+	if err != nil {
+		println(err.Error())
+	}
+
+	// TODO investigate reading into the buffer dynamically
+	responseBuf := bufio.NewReaderSize(conn, 8000)
+
+	for {
+
+		headerBytes := make([]byte, headerSize)
+		_, err := responseBuf.Read(headerBytes)
+		if err != nil {
+			println(err.Error())
+		}
+
+		header := &ResponseHeader{}
+		err = struc.Unpack(bytes.NewReader(headerBytes), header)
+		if err != nil {
+			println(err.Error())
+		}
+
+		if int(header.Status) != 0 || int(header.KeyLength) == 0 {
+			// TODO handle error cases more gracefully
+			break
+		}
+
+		body := make([]byte, header.TotalBodyLength)
+		_, err = responseBuf.Read(body)
+		if err != nil {
+			println(err.Error())
+		}
+
+		key := body[header.ExtrasLength:header.KeyLength]
+		value := body[header.KeyLength+header.ExtrasLength:]
+
+		fmt.Printf("%s: %s\n", string(key), string(value))
 	}
 }
